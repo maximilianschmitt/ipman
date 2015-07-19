84f0ee557c549ae1e3fead6de24ad9d2ff3f757d
Maximilian Schmitt <maximilian.schmitt@googlemail.com>
2015-07-19 14:45:14 +0200

    rough working solution

--- components/app.js
+++ components/app.js
@@ -7,6 +7,10 @@ const Link = Router.Link;
 
 class App extends React.Component {
   render() {
+    const currentRoutes = this.context.router.getCurrentRoutes();
+    const routeName = currentRoutes[currentRoutes.length - 1].name;
+    const routeProps = this.props.data[routeName];
+
     return (
       <html>
         <head>
@@ -18,11 +22,15 @@ class App extends React.Component {
             <li><Link to="home">Home</Link></li>
             <li><Link to="ip">Ip</Link></li>
           </ul>
-          <RouteHandler />
+          <RouteHandler {...routeProps} />
         </body>
       </html>
     );
   }
 }
+
+App.contextTypes = {
+  router: React.PropTypes.func.isRequired
+};
 
 module.exports = App;
